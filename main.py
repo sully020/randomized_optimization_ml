20d4f73ef379421340d41f76a71395e7d18cb1e9
Chris Sullivan <cs590919@wne.edu>
2023-03-25 18:18:03 -0400

    ADD: Preprocess data function

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import mlrose
-import pandas
+from mlrose import NeuralNetwork, random_hill_climb, simulated_annealing, genetic_alg
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+
+def preprocess_data():
+    patients = pd.read_csv('diabetes.csv')
+    x = patients.drop(columns = 'class_val')
+    y = patients['class_val']
+    scaler = MaxAbsScaler()
+    scaled_x = scaler.fit_transform(x)
+    xtrain, xtest, ytrain, ytest = train_test_split(scaled_x, y)
+    ml_data = (xtrain, xtest, ytrain, ytest)
+    return ml_data
+
 
 def choose_optimizer():
     choice = input("Please enter the number of the algorithm you would like to use:\n 1.) R.R. Hill Climbing 2.) Simulated Annealing\
@@ -7,4 +22,4 @@ def choose_optimizer():
 
 def optimize_weights():
     optimizer = choose_optimizer()
-    neural_net = mlrose.NeuralNetwork(67, algorithm = optimizer, clip_max = 1, random_state = 1, restarts = 100, schedule = 1, pop_size = 500, curve = True)
+    neural_net = NeuralNetwork(algorithm = optimizer, clip_max = 1, restarts = 100, schedule = 1, pop_size = 500, curve = True)
