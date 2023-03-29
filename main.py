10a55d31ece56d7b4939f3b677bd242125847099
Chris Sullivan <cs590919@wne.edu>
2023-03-28 20:11:01 -0400

    ADD: Implement algorithm choice, & weight optimization functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlrose import random_hill_climb, simulated_annealing, genetic_alg, NeuralNetwork, GeomDecay
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, accuracy_score
 import pandas as pd
 
 
@@ -17,29 +18,44 @@ def preprocess_data():
 
 
 def choose_weights_optimizer():
-    choice = input("Please enter the number of the algorithm you would like to use:\n 1.) R.R. Hill Climbing 2.) Simulated Annealing\
-     3.) Genetic Algorithm \n")
+    choice = input("Please enter the number of the algorithm you would like to use:\n1.) R.R. Hill Climbing 2.) Simulated Annealing\
+ 3.) Genetic Algorithm \n")
     
+    # Random state?
+
     match choice:
         case '1':
-            hill_climb_weights(data)
+            print("You selected: Random Restart Hill Climbing")
+            restarts = int(input("How many random restarts would you like to use? "))
+            neural = NeuralNetwork([67], algorithm = 'random_hill_climb', clip_max = 1, restarts = restarts,
+                max_iters = 500, random_state = 0, curve = True)
         case '2':
-            simulate_annealing_weights(data)
+            print("You selected: Simulated Annealing")
+            neural = NeuralNetwork([67], algorithm = 'simulated_annealing', clip_max = 1, schedule = GeomDecay(), 
+                max_iters = 500, random_state = 0, curve = True)
         case '3':
-            genetic_alg_weights(data)
+            print("You selected: Genetic Algorithm")
+            population = int(input("Please enter population size: "))
+            neural = NeuralNetwork([67], algorithm = 'genetic_alg', clip_max = 1, pop_size = population, 
+                mutation_prob = 0.7, max_iters = 500, random_state = 0, curve = True)
+    return neural
 
 
-def hill_climb_weights():
-    neural_net = NeuralNetwork([3], algorithm = random_hill_climb, clip_max = 1, restarts = 100, curve = True)
+def optimize_weights(data):
+    neural = choose_weights_optimizer()
+    neural.fit(data[0], data[2])       # Fit weights to training data using chosen optimization alg.
+    y_train_preds = neural.predict(data[0])
+    train_accuracy = f1_score(data[2], y_train_preds)
+    print("The F1-score training accuracy of the neural network was: " + "{:.2f}".format(train_accuracy) + "%")
+    y_test_preds = neural.predict(data[1])
+    test_accuracy = f1_score(data[3], y_test_preds)
+    print("The F1-score test accuracy of the neural network was: " + "{:.2f}".format(test_accuracy) + "%")
+    #print(neural.fitness_curve)
 
-def simulate_annealing_weights():
-    neural_net = NeuralNetwork([3], algorithm = simulated_annealing, clip_max = 1, schedule = GeomDecay(), curve = True)
-
-def genetic_alg_weights():
-    neural_net = NeuralNetwork([3], algorithm = genetic_alg, clip_max = 1, pop_size = 100, curve = True)
 
 def main():
-    pass
+    data = preprocess_data()
+    optimize_weights(data)
 
 if __name__ == "__main__":
     main()
